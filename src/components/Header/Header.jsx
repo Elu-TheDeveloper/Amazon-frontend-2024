fa7dcf300ce49e4504196ecae36b8a4334698f8e
Eliyas Daba <eladaba2022@gmail.com>
2024-12-06 14:47:23 +0300

    deployexist

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import{Link} from 'react-router-dom'
-import classes from "./style.css"
+import classes from "./style.module.css"
 import { BsSearch } from "react-icons/bs";
 import { PiShoppingCartSimpleLight } from "react-icons/pi";
 import { SlLocationPin } from "react-icons/sl";
